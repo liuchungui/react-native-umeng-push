ad9cdf3606429bd2115f67a82feac57c16db7073
liuchungui <404468494@qq.com>
2016-09-23 22:35:46 +0800

    update java code

--- Example/android/app/src/main/java/com/testproject/MainActivity.java
+++ Example/android/app/src/main/java/com/testproject/MainActivity.java
@@ -1,9 +1,25 @@
 package com.testproject;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
 import com.facebook.react.ReactActivity;
-import com.liuchungui.react_native_umeng_push.UmengPushPackage;
+
+import com.umeng.common.UmLog;
+import com.umeng.message.IUmengRegisterCallback;
+import com.umeng.message.MsgConstant;
+import com.umeng.message.PushAgent;
+import com.umeng.message.UTrack;
+import com.umeng.message.UmengMessageHandler;
+import com.umeng.message.UmengNotificationClickHandler;
+import com.umeng.message.entity.UMessage;
 
 public class MainActivity extends ReactActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        PushAgent.getInstance(this).onAppStart();
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
--- Example/android/app/src/main/java/com/testproject/MainApplication.java
+++ Example/android/app/src/main/java/com/testproject/MainApplication.java
@@ -8,13 +8,39 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.liuchungui.react_native_umeng_push.UmengPushApplication;
-import com.liuchungui.react_native_umeng_push.UmengPushPackage;
+
+import com.umeng.common.UmLog;
+import com.umeng.message.IUmengRegisterCallback;
+import com.umeng.message.MsgConstant;
+import com.umeng.message.PushAgent;
+import com.umeng.message.UTrack;
+import com.umeng.message.UmengMessageHandler;
+import com.umeng.message.UmengNotificationClickHandler;
+import com.umeng.message.entity.UMessage;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends UmengPushApplication implements ReactApplication {
+public class MainApplication extends Application implements ReactApplication {
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    PushAgent mPushAgent = PushAgent.getInstance(this);
+//注册推送服务，每次调用register方法都会回调该接口
+    mPushAgent.register(new IUmengRegisterCallback() {
+
+      @Override
+      public void onSuccess(String deviceToken) {
+        //注册成功会返回device token
+        Log.i("LCGTest", "deviceToken: " + deviceToken);
+      }
+
+      @Override
+      public void onFailure(String s, String s1) {
+
+      }
+    });
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -25,8 +51,7 @@ public class MainApplication extends UmengPushApplication implements ReactApplic
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new UmengPushPackage()
+          new MainReactPackage()
       );
     }
   };
